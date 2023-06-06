f6f4bdc1f0f4cc36fa4bd28da3f3f9232b5e21e9
DESKTOP-1MOOCM7\User <yz1410aa@gmail.com>
2023-06-06 17:17:25 +0800

    新增一些小東西

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,35 @@
 <script setup></script>
 
 <template>
-   <h1>問卷管理</h1>
-   <div class="search-area">
+  <div class="container">
+    <h1>問卷管理</h1>
+    <div class="search-area">
       <div class="title d-flex">
-         <p>問卷標題</p>
-         <input type="text" />
+        <p>問卷標題</p>
+        <input type="text" />
       </div>
       <div class="date d-flex">
-         <p>開始/結束</p>
-         <input type="date" />
-         <input type="date" />
+        <p>開始/結束</p>
+        <input type="date" />
+        <input type="date" />
       </div>
-   </div>
+    </div>
+    <div class="questionnaire-area">
+      <button>刪除</button>
+      <button>新增</button>
+      <div class="survey-table"></div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+.container {
+  margin: 4rem;
+}
 .search-area {
-   display: flex;
-   flex-direction: column;
-   margin: 4rem;
-   border: 1px solid;
+  display: flex;
+  flex-direction: column;
+
+  border: 1px solid;
 }
 </style>
