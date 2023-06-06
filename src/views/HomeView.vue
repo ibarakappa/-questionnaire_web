a4395b391dabdf0dbbb988d320be1957ae4bd768
DESKTOP-RRPRPE7\Lenovo <yz1410aa@gmail.com>
2023-06-06 12:30:00 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+   <h1>問卷管理</h1>
+   <div class="search-area">
+      <div class="title d-flex">
+         <p>問卷標題</p>
+         <input type="text" />
+      </div>
+      <div class="date d-flex">
+         <p>開始/結束</p>
+         <input type="date" />
+         <input type="date" />
+      </div>
+   </div>
+</template>
+
+<style lang="scss" scoped>
+.search-area {
+   display: flex;
+   flex-direction: column;
+   margin: 4rem;
+   border: 1px solid;
+}
+</style>
